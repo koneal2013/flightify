76cf9a38d4f5bb99ddd07aed049e991eb51bfd53
Kenston O'Neal <koneal2013@gmail.com>
2023-01-10 18:05:57 -0500

    removing println form recovery

--- middleware/recovery.go
+++ middleware/recovery.go
@@ -7,7 +7,6 @@ import (
 
 func Recovery(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		defer func() {
 			err := recover()
 			if err != nil {
